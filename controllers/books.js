608b7b386edb8500201ddacd75bf11e7f9269517
bkerr <bkerr@quantcast.com>
2018-04-09 12:03:22 -0700

    convert to backend API

--- controllers/books.js
+++ controllers/books.js
@@ -1,11 +1,26 @@
 const db = require('../models/index');
 
 const Books = {
-  index: (req, res) => {
+  list: (req, res) => {
     db.Book.find({}, (err, foundBooks) => {
-      console.log(foundBooks, err)
-      res.render('../views/books/index', {
-        books: foundBooks
+      res.json({
+        books: foundBooks.map((book) => {
+          return {
+            id: book.id,
+            title: book.title
+          }
+        })
+      });
+    });
+  },
+
+  recommendations: (req, res) => {
+    db.Book.findById(req.params.id, (err, foundBook) => {
+      if (err) {
+        return res.status(400).json({error: 'not found'})
+      }
+      res.json({
+        titles: foundBook.recommended_titles
       });
     });
   }
--- routes.js
+++ routes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const StaticPages = require('./controllers/static_pages');
 const Books = require('./controllers/books');
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', StaticPages.home);
-router.get('/select', Books.index);
+router.get('/api/books/list', Books.list);
+router.get('/api/books/:id/recommendations', Books.recommendations);
 
 module.exports = router;
