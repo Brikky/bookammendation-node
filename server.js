3f09b677d5c2917b924f3327de4c5c9528cb0881
bkerr <bkerr@quantcast.com>
2018-04-09 13:21:32 -0700

    add react frontend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = express();
 app.use(require('./routes'));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
